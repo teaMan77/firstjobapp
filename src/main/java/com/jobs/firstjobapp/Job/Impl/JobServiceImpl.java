87f5bcfe3c334a7085cfbc03ff7eae74cfdb3ab9
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-11-29 11:59:52 +0530

    Establishing database connetction for CRUD

--- src/main/java/com/jobs/firstjobapp/Job/Impl/JobServiceImpl.java
+++ src/main/java/com/jobs/firstjobapp/Job/Impl/JobServiceImpl.java
@@ -1,63 +1,64 @@
 package com.jobs.firstjobapp.Job.Impl;
 
 import com.jobs.firstjobapp.Job.Job;
+import com.jobs.firstjobapp.Job.JobRepository;
 import com.jobs.firstjobapp.Job.JobService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs = new ArrayList<Job>();
-    private int id = 1;
+//    private List<Job> jobs = new ArrayList<Job>();
+//    private int id = 1;
+
+    JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
 
     @Override
     public List<Job> findAll() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public String addJob(Job job) {
-        job.setId(id++);
-        jobs.add(job);
+        jobRepository.save(job);
         return "Job added successfully!";
     }
 
     @Override
     public Job findJobById(int id) {
-        for (Job job : jobs) {
-            if (job.getId() == id) {
-                return job;
-            }
-        }
-        return null;
+        return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public boolean deleteJobById(int id) {
-        for (Job job : jobs) {
-            if (job.getId() == id) {
-                jobs.remove(job);
-                return true;
-            }
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
 
     @Override
     public boolean updateJobById(int id, Job updatedJob) {
-        for (Job job : jobs) {
-            if (job.getId() == id) {
-//                BeanUtils.copyProperties(updatedJob, job);
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+                Job job = jobOptional.get();
                 job.setTitle(updatedJob.getTitle());
                 job.setDescription(updatedJob.getDescription());
                 job.setMinSalary(updatedJob.getMinSalary());
                 job.setMaxSalary(updatedJob.getMaxSalary());
                 job.setLocation(updatedJob.getLocation());
+                jobRepository.save(job);
                 return true;
-            }
         }
         return false;
     }
---
+++ src/main/java/com/jobs/firstjobapp/Job/JobRepository.java
@@ -0,0 +1,6 @@
+package com.jobs.firstjobapp.Job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Integer> {
+}
